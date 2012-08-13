03e8466ffecc6412819e34286946717619ff3a36
Manuel Reinhardt <Manuel.Reinhardt@neon-cathedral.net>
2012-08-13 13:05:53 +0100

    added MessageFactory

--- collective/team/__init__.py
+++ collective/team/__init__.py
@@ -1,5 +1,7 @@
 # -*- extra stuff goes here -*-
+from zope.i18nmessageid import MessageFactory
 
+TeamMessageFactory = MessageFactory('collective.team')
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
